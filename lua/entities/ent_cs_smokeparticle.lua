b5c99520fe67e82cd265f66d8d41d0dd1f58dad5
BurgerLUA <mattwasnothere@gmail.com>
2014-10-22 20:48:30 -0700

    Fixed Smoke

--- lua/entities/ent_cs_smokeparticle.lua
+++ lua/entities/ent_cs_smokeparticle.lua
@@ -16,7 +16,7 @@ function ENT:Initialize()
 		self:SetModel("models/Items/AR2_Grenade.mdl") 
 		self:PhysicsInitSphere( size, "wood" )
 		self:SetCollisionBounds( Vector( -size, -size, -size ), Vector( size, size, size ) )
-		
+		self:SetCollisionGroup(COLLISION_GROUP_DEBRIS)
 		
 		
 		local phys = self:GetPhysicsObject()

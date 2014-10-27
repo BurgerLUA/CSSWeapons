43212c53766bbff3527ed4502576416bab82360f
BurgerLUA <mattwasnothere@gmail.com>
2014-10-27 15:33:55 -0700

    Added missing .338 ammo

---
+++ lua/entities/ent_cs_ammo_338.lua
@@ -0,0 +1,15 @@
+ENT.Type 				= "anim"
+ENT.Base 				= "ent_cs_ammo_base"
+ENT.PrintName 			= ".338 (40)"
+ENT.Author 				= ""
+ENT.Information 		= ""
+
+ENT.Spawnable 			= true
+ENT.AdminSpawnable		= false 
+ENT.Category			= "CS:S Ammo"
+
+AddCSLuaFile()
+
+ENT.AmmoType 			= "SniperRound"
+ENT.AmmoAmount 			= 40
+ENT.AmmoModel			= "models/Items/BoxMRounds.mdl"
